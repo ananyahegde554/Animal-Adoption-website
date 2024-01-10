885565f0502cd1c8d21a06f9e1a8cd1edc3f8e25
ananyahegde554 <annahedlena@gmail.com>
2024-01-10 23:33:47 +0530

    app prototype

---
+++ UploadFilesApp/Data/PetModel.cs
@@ -0,0 +1,11 @@
+﻿namespace UploadFilesApp.Data;
+
+public class PetModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Age { get; set; }
+    public string? Type { get; set; }
+    public string? FileName { get; set; }
+    public string? OwnersContact { get; set; }
+}
